5a03b6c0b5962b9a71e0b9f443dd889749c6ec26
Victor Salome <victorsalome41@hotmail.com>
2024-02-28 15:59:33 -0300

    Criação do projeto - teste AZShip

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-center relative">
+      <div className="relative max-w-screen-2xl">
+        <ul className="links">
+          <li>
+            <Link to="/">Inicio</Link>
+          </li>
+          <li>
+            <Link to="/favorites">Favoritos</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
